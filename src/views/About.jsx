4441ed5a29fc27cbf650d5fe6e81b98bfe718fc1
gongx <gx_0408@163.com>
2022-04-21 19:46:15 +0800

    userEffect

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,8 +1,60 @@
 // About/index.jsx
-import React from 'react'
-
+import React, { useEffect, useState, useCallback, useMemo } from 'react'
+import { get } from '@/utils'
 export default function About() {
+  // useEffect === mounted（） componentDidMount 
+  /**
+   *  
+   * useEffect(() => {//生命周期副作用钩子
+     get('/index-infos').then(() => {
+     })
+    }, [])
+   * 
+
+    const handleClickButton2 = useCallback(() => {//只有在依赖项发生变化的时候才会更新（返回一个新的函数）。
+    setCount2(count2 + 1);      //侦听器
+    ....
+    }, [count2]);
+
+
+    const userInfo = useMemo(() => { //需要返回一个值，只有在依赖项发生改变的时候，才会重新调用此函数，返回一个新的值。
+    return {     //计算属性
+      // ...
+      name: "Jace",
+      age: count
+        };
+      }, [count]);
+   */
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  useEffect(() => {//生命周期副作用钩子
+    console.log(1);
+  }, [])
+  const handleClickButton2 = useCallback(() => {
+    setCount2(count2 + 1);
+    setCount(count + 1)
+  }, [count2]);
+  const userInfo = useMemo(() => {
+    return {
+      // ...
+      name: "Jace",
+      age: count
+    };
+  }, [count]);
+  // function handleClick() {
+  //   setA((a) => a += 1)
+  //   console.log(a);
+  // }
   return <div>
-    About
+    <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count + 1)}>
+      Click me
+    </button>
+    <button onClick={handleClickButton2}>
+      Click me2
+    </button>
+    <div className="">
+      {userInfo.name}
+    </div>
   </div>
 }
